3a1c215adcc233582072038d669f2b46b606510d
Christina Wu <wu_christina@outlook.com>
2022-07-03 16:21:07 -0400

    first iteration of a number guessing game using Node.js readline module

---
+++ guessing-game.js
@@ -0,0 +1,62 @@
+// import the readline module into our file
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let secretNumber;
+
+// Checks if number is close to the secretNumber and provides feedback
+function checkGuess (guess) {
+    if (secretNumber < guess) {
+        console.log("Too high");
+        return false;
+    } else if (secretNumber > guess) {
+        console.log("Too low");
+        return false;
+    } else {
+        console.log("Correct!");
+        return true;
+    };
+};
+
+// Ask user to enter a guess and checks if the guess is close to the secretNumber
+function askGuess (numGuess) {
+    rl.question("Enter a guess: ", numGuess => {
+        let stringToNum = Number(numGuess);
+        let check = checkGuess(stringToNum);
+        if (check === true) {
+            console.log("You win!");
+            rl.close();
+        } else {
+        askGuess();
+        };
+    });
+};
+
+// Ask user to enter min and max numbers
+function askRange () {
+    rl.question("Enter a min number: ", firstAnswer => {
+        // only after the user responds to question one, then ask question two
+        let minNum = Number(firstAnswer);
+        rl.question("Enter a max number: ", secondAnswer => {
+            let maxNum = Number(secondAnswer);
+
+            // assign secretNumber to a random number between min to max (inclusive)
+            secretNumber = randomInRange(minNum, maxNum);
+            console.log("I'm thinking of a number between " + firstAnswer + " and " + secondAnswer);
+
+            askGuess();
+        });
+    });
+};
+
+// Generates random whole number between min and max numbers (inclusive)
+function randomInRange (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+askRange();
