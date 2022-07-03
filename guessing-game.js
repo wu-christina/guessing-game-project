4d1990b385f9a3e9bf8cc1d2b44964b7b0b47169
Christina Wu <wu_christina@outlook.com>
2022-07-03 16:46:31 -0400

    Second iteration - Refractor askGuess function to limit player to 5 attempts

--- guessing-game.js
+++ guessing-game.js
@@ -7,31 +7,37 @@ const rl = readline.createInterface({
 });
 
 let secretNumber;
+let numAttempts = 5; // number of attemps to guess the secretNumber;
 
 // Checks if number is close to the secretNumber and provides feedback
 function checkGuess (guess) {
     if (secretNumber < guess) {
-        console.log("Too high");
+        console.log("Too high.");
         return false;
     } else if (secretNumber > guess) {
-        console.log("Too low");
+        console.log("Too low.");
         return false;
     } else {
-        console.log("Correct!");
+        console.log("You got it!");
         return true;
     };
 };
 
 // Ask user to enter a guess and checks if the guess is close to the secretNumber
 function askGuess (numGuess) {
+    numAttempts--;
+
     rl.question("Enter a guess: ", numGuess => {
         let stringToNum = Number(numGuess);
         let check = checkGuess(stringToNum);
         if (check === true) {
-            console.log("You win!");
+            console.log("You win! 😀");
             rl.close();
-        } else {
+        } else if (numAttempts > 0) {
         askGuess();
+        } else if (numAttempts === 0) {
+            console.log("You Lose 🙁");
+            rl.close();
         };
     });
 };
@@ -46,7 +52,7 @@ function askRange () {
 
             // assign secretNumber to a random number between min to max (inclusive)
             secretNumber = randomInRange(minNum, maxNum);
-            console.log("I'm thinking of a number between " + firstAnswer + " and " + secondAnswer);
+            console.log("I'm thinking of a number between " + firstAnswer + " and " + secondAnswer + ". 🤔");
 
             askGuess();
         });
@@ -58,5 +64,5 @@ function randomInRange (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
+// To start the game
 askRange();
